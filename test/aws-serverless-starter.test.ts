828d891bd30870ecbfd9f981481dadb48430c0eb
eechih <eechih@gmail.com>
2023-09-19 11:58:10 +0800

    Adding prettier, lint-staged

--- test/aws-serverless-starter.test.ts
+++ test/aws-serverless-starter.test.ts
@@ -1,17 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as AwsServerlessStarter from '../lib/aws-serverless-starter-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Template, Match } from 'aws-cdk-lib/assertions'
+import * as AwsServerlessStarter from '../lib/aws-serverless-starter-stack'
 
 test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
+  const app = new cdk.App()
   // WHEN
-  const stack = new AwsServerlessStarter.AwsServerlessStarterStack(app, 'MyTestStack');
+  const stack = new AwsServerlessStarter.AwsServerlessStarterStack(
+    app,
+    'MyTestStack'
+  )
   // THEN
 
-  const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack)
 
   template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
-});
+    VisibilityTimeout: 300,
+  })
+  template.resourceCountIs('AWS::SNS::Topic', 1)
+})
