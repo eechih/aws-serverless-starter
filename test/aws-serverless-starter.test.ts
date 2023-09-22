78893d0647b4db904c6e90ed11ab481bb1a7c4ab
eechih <eechih@gmail.com>
2023-09-22 15:00:31 +0800

    fix

--- test/aws-serverless-starter.test.ts
+++ test/aws-serverless-starter.test.ts
@@ -2,10 +2,18 @@ import * as cdk from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
 import { ServiceStack } from '../lib/service-stack'
 
+import config from '../lib/config'
+
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App()
   // WHEN
-  const stack = new ServiceStack(app, 'MyTestStack', { stageName: 'test' })
+  const stack = new ServiceStack(app, 'MyTestStack', {
+    stageName: 'test',
+    env: {
+      account: config.deployAccount,
+      region: config.deployRegion,
+    },
+  })
   // THEN
 
   const template = Template.fromStack(stack)
